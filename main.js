fc4d89c754a92d6605cf9836c39b2754300219c8
haimilo <ngotuehai97@gmail.com>
2021-09-11 21:14:25 +0700

    add file

---
+++ main.js
@@ -0,0 +1,13 @@
+/*
+Xây dựng chức năng cần làm
+ * Render Song
+ * Scroll top
+ * Play/ Pause/ Seek
+ * CD Rotate
+ * Next/ Previous
+ * Random
+ * Next/ Repeat when ended
+ * Active song
+ * Scroll active song into view
+ * Play song when click
+*/
